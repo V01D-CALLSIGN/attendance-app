ce559aaf6d6d43ef729e7138a88559682ec559b7
V01D-CALLSIGN <193118344+V01D-CALLSIGN@users.noreply.github.com>
2026-08-28 16:11:51 -0500

    Initial Commit

---
+++ attendance-app/attendance_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  attendance_appApp.swift
+//  attendance-app
+//
+//  Created by Anirban Bagchi on 8/28/26.
+//
+
+import SwiftUI
+
+@main
+struct attendance_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
